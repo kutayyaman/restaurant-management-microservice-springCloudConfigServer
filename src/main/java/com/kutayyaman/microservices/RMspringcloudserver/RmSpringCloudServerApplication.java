7f13b2dfe7e7782c73591f3ac954f7b93f63416c
Kutay Yaman <yamankutay1@gmail.com>
2021-12-10 20:19:53 +0300

    Project initialized

---
+++ src/main/java/com/kutayyaman/microservices/RMspringcloudserver/RmSpringCloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.kutayyaman.microservices.RMspringcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmSpringCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmSpringCloudServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kutayyaman/microservices/RMspringcloudserver/RmSpringCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutayyaman.microservices.RMspringcloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmSpringCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
