e92ee0b2d37552a273fe3de899569ac8fb850080
Kutay Yaman <yamankutay1@gmail.com>
2021-12-10 21:31:54 +0300

    RmSpringCloudServer is linked with properties git repo

--- src/main/java/com/kutayyaman/microservices/RMspringcloudserver/RmSpringCloudServerApplication.java
+++ src/main/java/com/kutayyaman/microservices/RMspringcloudserver/RmSpringCloudServerApplication.java
@@ -2,8 +2,10 @@ package com.kutayyaman.microservices.RMspringcloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class RmSpringCloudServerApplication {
 
 	public static void main(String[] args) {
